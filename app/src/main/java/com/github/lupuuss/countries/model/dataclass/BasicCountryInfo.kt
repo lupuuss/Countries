cd6beaee668f6ffc529ba28092d85356eff3871d
Lupuss <damian.baczynski@gmail.com>
2019-05-01 17:31:50 +0200

    Overrides BasicCountryInfo.toString

--- app/src/main/java/com/github/lupuuss/countries/model/dataclass/BasicCountryInfo.kt
+++ app/src/main/java/com/github/lupuuss/countries/model/dataclass/BasicCountryInfo.kt
@@ -3,4 +3,8 @@ package com.github.lupuuss.countries.model.dataclass
 class BasicCountryInfo(
     val name: String,
     val flag: String
-)
+) {
+    override fun toString(): String {
+        return "[$name, $flag]"
+    }
+}
