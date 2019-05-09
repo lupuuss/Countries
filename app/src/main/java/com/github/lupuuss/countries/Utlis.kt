28c008ef69ca33f254dc2883e058e1922418fb1a
Lupuss <damian.baczynski@gmail.com>
2019-05-09 10:36:38 +0200

    Fix map zoom

--- app/src/main/java/com/github/lupuuss/countries/Utlis.kt
+++ app/src/main/java/com/github/lupuuss/countries/Utlis.kt
@@ -21,6 +21,9 @@ fun <T> flatList(list: List<T>, transformer: (T) -> String): String {
 
 fun formatAny(any: Any): String = NumberFormat.getInstance().format(any)
 
+/**
+ * Calculates zoom by interpolation for some country samples
+ */
 fun calculateZoom(rawCountryDetails: RawCountryDetails): Float {
 
     val area = rawCountryDetails.area
@@ -28,7 +31,7 @@ fun calculateZoom(rawCountryDetails: RawCountryDetails): Float {
     return when {
         area == 0.0 -> 7f
         area <= 500 -> (11.0024 - 0.00534691 * area).toFloat()
-        area in 500.0..28_000.0 -> (7f - 0.00105 * area).toFloat()
+        area in 500.0..28_000.0 -> (8f - 0.000055 * area).toFloat()
         area in 28_000.0..1_000_000.0 -> (6f - 2.0E-6 * area).toFloat()
         else -> (4.625 - 1.25E-7 * area).toFloat()
 
