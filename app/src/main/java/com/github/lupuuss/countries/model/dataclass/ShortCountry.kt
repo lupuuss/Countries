c7df116cf6b85d4eeae9118d2771c24937bfdcfd
Lupuss <damian.baczynski@gmail.com>
2019-05-10 11:50:06 +0200

    Removes unused method

--- app/src/main/java/com/github/lupuuss/countries/model/dataclass/ShortCountry.kt
+++ app/src/main/java/com/github/lupuuss/countries/model/dataclass/ShortCountry.kt
@@ -3,8 +3,4 @@ package com.github.lupuuss.countries.model.dataclass
 open class ShortCountry(
     val name: String,
     val alpha3Code: String
-) {
-    override fun toString(): String {
-        return "[$name]"
-    }
-}
+)
