5e32a8df56a48d199a162a3fd04275fad18a7e30
Lupuss <damian.baczynski@gmail.com>
2019-05-10 14:39:56 +0200

    Fix bug in AntiSpam

--- app/src/main/java/com/github/lupuuss/countries/kotlin/AntiSpam.kt
+++ app/src/main/java/com/github/lupuuss/countries/kotlin/AntiSpam.kt
@@ -1,13 +1,21 @@
 package com.github.lupuuss.countries.kotlin
 
+import timber.log.Timber
+
 class AntiSpam(private val timeProvider: () -> Long) {
 
     private val lastClick: MutableMap<String, Long> = mutableMapOf()
 
     fun doAction(identifier: String, minDeltaTime: Long, action: () -> Unit) {
 
-        val lastTime = lastClick.getOrElse(identifier) { 0L }
+        val lastTime = lastClick[identifier]
         val time = timeProvider()
+
+        if (lastTime == null) {
+            action()
+            lastClick[identifier] = time
+            return
+        }
 
         if ( time - lastTime > minDeltaTime) {
 
